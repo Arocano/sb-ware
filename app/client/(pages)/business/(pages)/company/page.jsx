2cf5009ee8180ce01ccdad5982bf4be561433226
Farid Ruano <fruanocm2777@gmail.com>
2024-06-25 18:49:57 -0500

    Update Company

--- app/client/(pages)/business/(pages)/company/page.jsx
+++ app/client/(pages)/business/(pages)/company/page.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 
 const Company = () => {
+
+  /* Plans */
+  const [planCont, setPlanCont] = useState(1)
+
+  const handlePlanConst = (op) => {
+    if(planCont === op) {
+      return
+    }else{
+      setPlanCont(op)
+    }
+  }
+
   return (
-    <div>Company</div>
+    <>
+      <section className='plans-section'>
+        <div className={planCont === 1 ?"container-option":"container-option gray"}>
+          <div className="options-body">
+            <div className={planCont === 1 ?'option active':'option'} onClick={()=>handlePlanConst(1)}/>
+            <div className={planCont === 2 ?'option active':'option'} onClick={()=>handlePlanConst(2)}/>
+          </div>
+          <div className={planCont === 1 ? "container a active" : "container a"}>
+            <div className="container-body">
+              <span className='title-body'>
+                Planes
+              </span>
+              
+            </div>
+          </div>
+          <div className={planCont === 2 ? "container b active" : "container b"}>
+            <div className="container-body">
+              <span className='title-body'>
+                Agregar
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="container-option">
+          Table
+        </div>
+      </section>
+    </>
   )
 }
 
